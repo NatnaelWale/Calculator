faa835621bc7bee950fd0751f6c41b1190a71678
NatnaelWale <nhattyjeje@gmail.com>
2024-04-29 10:43:24 -0400

    Added Error handling techniques

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ function addOnClick() {
   var number1Field = document.getElementById("number1Field").value;
   var number2Field = document.getElementById("number2Field").value;
 
+  if (isNaN(number1Field) || isNaN(number2Field)) {
+    document.getElementById("answerField").value = "";
+    document.getElementById("messagePara").innerHTML = 
+    "One or more of your input values are invalid";
+    return; // exit the addBtnClicked function
+    }
+
   var result = Number(number1Field) + Number(number2Field);
 
   document.getElementById("answerField").value = result;
+
+  document.getElementById("messagePara").innerHTML = "";
 }
 
 function subtractOnClick() {
